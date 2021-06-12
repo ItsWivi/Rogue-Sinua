ddec97bf2c1038cacc0eb6eb961d7806236e5d06
KorinMan9 <53703871+KorinMan9@users.noreply.github.com>
2021-06-11 22:07:00 -0400

    Fix player Movement
    Made it so that the player no longer cllips awkwardly into the ground

--- Assets/Scripts/TestController.cs
+++ Assets/Scripts/TestController.cs
@@ -5,6 +5,14 @@ using UnityEngine.AI;
 
 public class TestController : MonoBehaviour
 {
+
+
+    public NavMeshAgent playerNavMeshAgent;
+
+    public Camera playerCamera;
+
+
+
     //These are for HealthBar
     public int maxHealth = 100;
     public int currentHealth;
@@ -30,53 +38,24 @@ public class TestController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetMouseButton(1))
+        {
+            Ray myRay = playerCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+        
+            if(Physics.Raycast(myRay,out hit))
+            {
+                playerNavMeshAgent.SetDestination(hit.point);
+            }
+        }
+        
+        
         //Debug for taking damage
         // /*
         if (Input.GetKeyDown(KeyCode.Space))
             TakeDamage(20);
-        // */
-
-        if (Input.GetMouseButtonDown(1))
-            SetTargetPosition();
-
-        if (moving)
-            Move();
-    }
-
-    void SetTargetPosition()
-    {
-        //Rays translate the mouse clicking on the monitor to a point in the game
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 100))
-        {
-            // Set targetPosition to Ray's point
-            targetPosition = hit.point;
-
-            //The hero rotates towards targetPosition
-            lookAtTarget = new Vector3(targetPosition.x - transform.position.x,
-                transform.position.y,
-                targetPosition.z - transform.position.z);
-            playerRot = Quaternion.LookRotation(lookAtTarget);
-
-            //moving is set to true so that the Move function will run
-            moving = true;
-        }
-    }
-
-    void Move()
-    {
-        //The hero moves to targetPosition
-        transform.rotation = Quaternion.Slerp(transform.rotation,
-                                                playerRot,
-                                                rotSpeed * Time.deltaTime);
-        transform.position = Vector3.MoveTowards(transform.position,
-                                                targetPosition,
-                                                speed * Time.deltaTime);
-        if (transform.position == targetPosition)
-            //The hero stops moving once it reaches its destinatation
-            moving = false;
+     
     }
 
     void TakeDamage(int damage)
